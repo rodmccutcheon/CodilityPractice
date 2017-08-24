8467b9f769e586779257c947c2b73ed265fd33a0
Rod McCutcheon <rodmccutcheon@gmail.com>
2017-08-24 15:52:11 +1000

    Optimised performance of MaxCounters implementation and added some comments.

--- src/main/java/com/rodmccutcheon/lesson4countingelements/MaxCounters.java
+++ src/main/java/com/rodmccutcheon/lesson4countingelements/MaxCounters.java
@@ -4,8 +4,40 @@ public class MaxCounters {
 
     public int[] solution(int N, int[] A) {
         int[] countersAnswer = new int[N];
+
+        int max = 0;
+        int min = 0;
+
+        // loop through the operations
         for (int a : A) {
-            countersAnswer[a - 1] = countersAnswer[a - 1] + 1;
+            // increase the specified counter by one
+            if (a >= 1 && a <= N) {
+                int current = countersAnswer[a - 1];
+
+                // if this counter is below the min set it to the min before applying the increase operation
+                if (current < min) {
+                    current = min;
+                }
+
+                countersAnswer[a - 1] = ++current;
+
+                // keep track of the max counter
+                if (current > max) {
+                    max = current;
+                }
+            }
+            // set all counters to max
+            // for better performance, rather than apply this across all counters now, we maintain the 'min' to add to
+            // subsequent operations
+            else if (a == N + 1) {
+                min = max;
+            }
+        }
+        // final check to make sure no counters are below the min
+        for (int i = 0; i < countersAnswer.length; i++) {
+            if (countersAnswer[i] < min) {
+                countersAnswer[i] = min;
+            }
         }
         return countersAnswer;
     }
--- src/test/java/com/rodmccutcheon/lesson4countingelements/MaxCountersTest.java
+++ src/test/java/com/rodmccutcheon/lesson4countingelements/MaxCountersTest.java
@@ -18,4 +18,25 @@ public class MaxCountersTest {
         int[] expectedSolution = { 1, 0 };
         assertArrayEquals(expectedSolution, new MaxCounters().solution(2, A));
     }
+
+    @Test
+    void twoCountersMultipleAdditions() {
+        int[] A = { 1, 2, 1 };
+        int[] expectedSolution = { 2, 1 };
+        assertArrayEquals(expectedSolution, new MaxCounters().solution(2, A));
+    }
+
+    @Test
+    void twoCountersMaxCounter() {
+        int[] A = { 1, 3, 1, 1, 2 };
+        int[] expectedSolution = { 3, 2 };
+        assertArrayEquals(expectedSolution, new MaxCounters().solution(2, A));
+    }
+
+    @Test
+    void sample() {
+        int[] A = { 3, 4, 4, 6, 1, 4, 4 };
+        int[] expectedSolution = { 3, 2, 2, 4, 2 };
+        assertArrayEquals(expectedSolution, new MaxCounters().solution(5, A));
+    }
 }

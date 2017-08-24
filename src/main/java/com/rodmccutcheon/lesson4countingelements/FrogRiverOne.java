aa0ef6d47f7eb222a62546a7f5b092240a1d4dee
Rod McCutcheon <rodmccutcheon@gmail.com>
2017-08-24 12:53:44 +1000

    Fixed bug with FrogRiverOne solution where array index was out of bounds.

--- src/main/java/com/rodmccutcheon/lesson4countingelements/FrogRiverOne.java
+++ src/main/java/com/rodmccutcheon/lesson4countingelements/FrogRiverOne.java
@@ -4,7 +4,7 @@ public class FrogRiverOne {
 
     public int solution(int X, int[] A) {
         int gapsRemaining = X;
-        int[] count = new int[A.length];
+        int[] count = new int[X];
         for (int i = 0; i < A.length; i++) {
             if (count[A[i]-1] == 0) {
                 count[A[i]-1] = 1;
--- src/test/java/com/rodmccutcheon/lesson4countingelements/FrogRiverOneTest.java
+++ src/test/java/com/rodmccutcheon/lesson4countingelements/FrogRiverOneTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FrogRiverOneTest {
 
     @Test
-    void oneLeafSuccessfulCrossing() {
+    void singleLeafSuccessfulCrossing() {
         int[] A = { 1 };
         assertEquals(0, new FrogRiverOne().solution(1, A));
     }
@@ -16,5 +16,17 @@ public class FrogRiverOneTest {
     void twoLeavesUnsuccessfulCrossing() {
         int[] A = { 1, 1 };
         assertEquals(-1, new FrogRiverOne().solution(2, A));
+    }
+
+    @Test
+    void multipleLeavesSuccessfulCrossing() {
+        int[] A = { 1, 3, 1, 4, 2, 3, 5, 4 };
+        assertEquals(6, new FrogRiverOne().solution(5, A));
+    }
+
+    @Test
+    void singleLeafUnsuccessfulCrossing() {
+        int[] A = { 3 };
+        assertEquals(-1, new FrogRiverOne().solution(5, A));
     }
 }

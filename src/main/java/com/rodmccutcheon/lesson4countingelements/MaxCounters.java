86a992cea705999662d717b57c631dcd908cc205
Rod McCutcheon <rodmccutcheon@gmail.com>
2017-08-24 14:44:23 +1000

    Changing implementation of MaxCounters to allow for multiple counters.

--- src/main/java/com/rodmccutcheon/lesson4countingelements/MaxCounters.java
+++ src/main/java/com/rodmccutcheon/lesson4countingelements/MaxCounters.java
@@ -3,7 +3,10 @@ package com.rodmccutcheon.lesson4countingelements;
 public class MaxCounters {
 
     public int[] solution(int N, int[] A) {
-        int[] countersAnswer = { 1 };
+        int[] countersAnswer = new int[N];
+        for (int a : A) {
+            countersAnswer[a - 1] = countersAnswer[a - 1] + 1;
+        }
         return countersAnswer;
     }
 }
--- src/test/java/com/rodmccutcheon/lesson4countingelements/MaxCountersTest.java
+++ src/test/java/com/rodmccutcheon/lesson4countingelements/MaxCountersTest.java
@@ -11,4 +11,11 @@ public class MaxCountersTest {
         int[] A = { 1 };
         assertArrayEquals(A, new MaxCounters().solution(1, A));
     }
+
+    @Test
+    void twoCountersOneAddition() {
+        int[] A = { 1 };
+        int[] expectedSolution = { 1, 0 };
+        assertArrayEquals(expectedSolution, new MaxCounters().solution(2, A));
+    }
 }

8f47e493a1a784c65c4b7ec34471dff7c1cb2373
Rod McCutcheon <rodmccutcheon@gmail.com>
2017-08-27 13:03:13 +1000

    Adding extra tests and implementing max pairs functionality.

--- src/main/java/com/rodmccutcheon/lesson5prefixsums/PassingCars.java
+++ src/main/java/com/rodmccutcheon/lesson5prefixsums/PassingCars.java
@@ -2,15 +2,20 @@ package com.rodmccutcheon.lesson5prefixsums;
 
 public class PassingCars {
 
-    int solution(int A[], int N) {
-        int[] prefixSums = new int[N];
+    public static final int MAX_PAIRS = 1_000_000_000;
+
+    int solution(int A[]) {
+        int[] prefixSums = new int[A.length];
         int carsTravellingEast = 0;
-        for (int i = 1; i < N; i++) {
+        for (int i = 1; i < A.length; i++) {
             if (A[i - 1] == 0) {
                 carsTravellingEast++;
             }
             prefixSums[i] = prefixSums[i - 1] + (A[i] * carsTravellingEast);
+            if (prefixSums[i] > MAX_PAIRS) {
+                return -1;
+            }
         }
-        return prefixSums[N - 1];
+        return prefixSums[A.length - 1];
     }
 }
--- src/test/java/com/rodmccutcheon/lesson5prefixsums/PassingCarsTest.java
+++ src/test/java/com/rodmccutcheon/lesson5prefixsums/PassingCarsTest.java
@@ -2,19 +2,48 @@ package com.rodmccutcheon.lesson5prefixsums;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.IntStream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PassingCarsTest {
 
     @Test
-    void noPassingCars() {
+    void oneCarTravellingEast() {
         int[] A = { 0 };
-        assertEquals(0, new PassingCars().solution(A, A.length));
+        assertEquals(0, new PassingCars().solution(A));
+    }
+
+    @Test
+    void oneCarTravellingWest() {
+        int[] A = { 1 };
+        assertEquals(0, new PassingCars().solution(A));
     }
 
     @Test
     void onePassingCar() {
         int[] A = { 0, 1 };
-        assertEquals(1, new PassingCars().solution(A, A.length));
+        assertEquals(1, new PassingCars().solution(A));
+    }
+
+    @Test
+    void noPassingCars() {
+        int[] A = { 1, 0 };
+        assertEquals(0, new PassingCars().solution(A));
+    }
+
+    @Test
+    void multipleCarsTravellingEastAndWest() {
+        int[] A = { 0, 1, 0, 1, 1 };
+        assertEquals(5, new PassingCars().solution(A));
+    }
+
+    @Test
+    void maxPairs() {
+        IntStream i1 = IntStream.generate(() -> 0).limit(32_000);
+        IntStream i2 = IntStream.generate(() -> 1).limit(32_000);
+        int[] A = IntStream.concat(i1, i2).toArray();
+        assertEquals(-1, new PassingCars().solution(A));
+
     }
 }

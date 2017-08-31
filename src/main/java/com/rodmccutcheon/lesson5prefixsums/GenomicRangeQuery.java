a030b5a66fc95ccadd48588839b439b8a9ee7a6a
Rod McCutcheon <rodmccutcheon@gmail.com>
2017-08-31 16:18:18 +1000

    Fixed GenomicRangeQuery solution.

--- src/main/java/com/rodmccutcheon/lesson5prefixsums/GenomicRangeQuery.java
+++ src/main/java/com/rodmccutcheon/lesson5prefixsums/GenomicRangeQuery.java
@@ -2,9 +2,56 @@ package com.rodmccutcheon.lesson5prefixsums;
 
 public class GenomicRangeQuery {
 
+    public enum Nucleotide {
+        A(1), C(2), G(3), T(4);
+
+        private int impactFactor;
+
+        private Nucleotide(int impactFactor) {
+            this.impactFactor = impactFactor;
+        }
+
+        public int getImpactFactor() {
+            return this.impactFactor;
+        }
+    }
+
     public int[] solution(String S, int[] P, int[] Q) {
         int[] answers = new int[P.length];
-        answers[0] = 1;
+        int[][] prefixSums = new int[3][S.length() + 1];
+
+        for (int i = 0; i < S.length(); i++) {
+            short a = 0, c = 0, g = 0;
+            Nucleotide n = Nucleotide.valueOf(String.valueOf(S.charAt(i)));
+            switch (n) {
+                case A:
+                    a = 1;
+                    break;
+                case C:
+                    c = 1;
+                    break;
+                case G:
+                    g = 1;
+                    break;
+            }
+            prefixSums[0][i + 1] = prefixSums[0][i] + a;
+            prefixSums[1][i + 1] = prefixSums[1][i] + c;
+            prefixSums[2][i + 1] = prefixSums[2][i] + g;
+        }
+
+        for (int j = 0; j < P.length; j++) {
+            if (prefixSums[0][Q[j] + 1] - prefixSums[0][P[j]] >= 1) {
+                answers[j] = Nucleotide.A.getImpactFactor();
+            } else if (prefixSums[1][Q[j] + 1] - prefixSums[1][P[j]] >= 1) {
+                answers[j] = Nucleotide.C.getImpactFactor();
+            } else if (prefixSums[2][Q[j] + 1] - prefixSums[2][P[j]] >= 1) {
+                answers[j] = Nucleotide.G.getImpactFactor();
+            } else {
+                answers[j] = Nucleotide.T.getImpactFactor();
+            }
+        }
+
         return answers;
     }
 }
+
--- src/test/java/com/rodmccutcheon/lesson5prefixsums/GenomicRangeQueryTest.java
+++ src/test/java/com/rodmccutcheon/lesson5prefixsums/GenomicRangeQueryTest.java
@@ -8,9 +8,37 @@ public class GenomicRangeQueryTest {
 
     @Test
     void simple() {
-        int[] P = { 1 };
-        int[] Q = { 1 };
+        String dnaSequence = "A";
+        int[] P = { 0 };
+        int[] Q = { 0 };
         int[] answer = { 1 };
-        assertArrayEquals(answer, new GenomicRangeQuery().solution("A", P, Q));
+        assertArrayEquals(answer, new GenomicRangeQuery().solution(dnaSequence, P, Q));
+    }
+
+    @Test
+    void name() {
+        String dnaSequence = "TG";
+        int[] P = { 0 };
+        int[] Q = { 1 };
+        int[] answer = { 3 };
+        assertArrayEquals(answer, new GenomicRangeQuery().solution(dnaSequence, P, Q));
+    }
+
+    @Test
+    void sample() {
+        String dnaSequence = "CAGCCTA";
+        int[] P = { 2, 5, 0 };
+        int[] Q = { 4, 5, 6 };
+        int[] answer = { 2, 4, 1 };
+        assertArrayEquals(answer, new GenomicRangeQuery().solution(dnaSequence, P, Q));
+    }
+
+    @Test
+    void name2() {
+        String dnaSequence = "AC";
+        int[] P = { 0, 0, 1 };
+        int[] Q = { 0, 1, 1 };
+        int[] answer = { 1, 1, 2 };
+        assertArrayEquals(answer, new GenomicRangeQuery().solution(dnaSequence, P, Q));
     }
 }

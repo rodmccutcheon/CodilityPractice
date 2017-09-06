d38c8ffae320714de74713d48e472b903b3ee22b
Rod McCutcheon <rodmccutcheon@gmail.com>
2017-09-06 16:58:30 +1000

    Adding basic solution for Distinct problem.

---
+++ src/main/java/com/rodmccutcheon/lesson6sorting/Distinct.java
@@ -0,0 +1,8 @@
+package com.rodmccutcheon.lesson6sorting;
+
+public class Distinct {
+
+    public int solution(int[] A) {
+        return 1;
+    }
+}
---
+++ src/test/java/com/rodmccutcheon/lesson6sorting/DistinctTest.java
@@ -0,0 +1,20 @@
+package com.rodmccutcheon.lesson6sorting;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DistinctTest {
+
+    @Test
+    void testOneDistinctValue() {
+        int[] A = { 7 };
+        assertEquals(1, new Distinct().solution(A));
+    }
+
+    /*@Test
+    void testSortedArray() {
+        int[] A = { 1, 1, 2, 3, 3, 3 };
+        assertEquals(3, new Distinct().solution(A));
+    }*/
+}
